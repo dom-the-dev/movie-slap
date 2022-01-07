a541afa80f26f44ed463c977c28ac2ec42072408
Dominik <hi@domthedev.com>
2022-01-08 00:11:54 +0100

    mobile header size

--- components/SimpleHeader.js
+++ components/SimpleHeader.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const SimpleHeader = ({text, textColor}) => {
     return (
-        <div className={`h-52 flex items-center justify-center text-mid`}>
+        <div className={`h-20 md:h-52 flex items-center justify-center text-mid`}>
             <h1 className={`${textColor}`}>{text}</h1>
         </div>
     );
