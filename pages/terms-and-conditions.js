ba39a543f1e2c545e6d2a8855b916e1ccf85076b
Dominik <hi@domthedev.com>
2022-01-11 18:42:33 +0100

    add terms and conditions page

---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "../components/Layout";
+
+const Terms = () => {
+    return (
+        <Layout title={"Terms and Conditions"}>
+         <h1>Terms and conditions</h1>
+        </Layout>
+    );
+};
+
+export default Terms;
