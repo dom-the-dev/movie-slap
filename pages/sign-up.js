ff5804d0fb5974c01013c48d64b35a3e25ee7e7a
Dominik <hi@domthedev.com>
2022-01-12 07:33:48 +0100

    add links to terms on sign up page

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -63,7 +63,7 @@ const SignUp = ({logout}) => {
                         onChange={e => setPassword(e.target.value)}
                     />
 
-                    <label htmlFor="terms">I accept the terms and conditions</label>
+                    <label htmlFor="terms">I accept the <Link href="/terms-and-conditions"><a>terms and conditions</a></Link></label>
                     <input type="checkbox" id="terms" name={"terms"} required/>
 
                     <button className={`primary mt-1`} type={"submit"}>Send</button>
--- pages/terms-and-conditions.js
+++ pages/terms-and-conditions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from "../components/Layout";
+import Link from 'next/link'
 
 const Terms = () => {
     return (
