ff28ec528e08949fb3617bd2aab5eb0cb0b628c7
Dominik <hi@domthedev.com>
2022-02-04 11:31:16 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,47 @@
+import {useEffect} from "react";
 import '../styles/globals.scss'
 import "../components/movieCard.css"
 import "../node_modules/slick-carousel/slick/slick.css";
 import "../node_modules/slick-carousel/slick/slick-theme.css";
 import {supabase} from "../lib/initSupabase";
 import {UserContextProvider} from "../lib/UserContext";
+import * as gtag from "../lib/gtag"
+import {useRouter} from "next/router";
+import Script from "next/script";
 
 function MyApp({Component, pageProps}) {
+    const router = useRouter()
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url)
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange)
+        }
+    }, [router.events]);
+
     return (
         <UserContextProvider supabaseClient={supabase}>
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+            />
             <Component {...pageProps} />
         </UserContextProvider>
     )
